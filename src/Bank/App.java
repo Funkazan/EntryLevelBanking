dc6d9d45a6d247e7d11b81b144898a50227497eb
Funkazan <128518893+Funkazan@users.noreply.github.com>
2023-12-30 00:59:17 +0100

    Entry level Banking app

--- src/Bank/App.java
+++ src/Bank/App.java
@@ -59,31 +59,36 @@ public class App {
                     System.out.println("Registration successful! Your details: ");
                     System.out.println(newCustomer.toString());
                 } else if ("1".equals(response)) {
+                    System.out.println("Please enter the AccountID of the sender.");
+                    String senderIdInput = scanner.nextLine();
                     System.out.println("Please enter the AccountID of the receiver.");
-                    String accountIdInput = scanner.nextLine();
+                    String receiverIdInput = scanner.nextLine();
+                    System.out.println("Please enter the amount of the money which is to be transfered.");
+                    String transferAmountInput = scanner.nextLine();
                     // Convert the user input to an Integer
-                    int accountId;
+                    int senderId, receiverId, transferAmount;
                     while (true) {
                         try {
-                            accountId = Integer.parseInt(accountIdInput);
+                            senderId = Integer.parseInt(senderIdInput);
+                            receiverId = Integer.parseInt(receiverIdInput);
+                            transferAmount = Integer.parseInt(transferAmountInput);
                             break; // Exit the loop if parsing is successful
                         } catch (NumberFormatException e) {
-                            System.err.println("Invalid input. Please enter an Integer for the balance.");
-                            accountIdInput = scanner.nextLine(); // Ask for input again
+                            System.err.println(
+                                    "Invalid input. Please enter an Integer for the sender, receiver and amount.");
+                            senderIdInput = scanner.nextLine();
+                            receiverIdInput = scanner.nextLine();
+                            transferAmountInput = scanner.nextLine();
                         }
                     }
-                    Customer sender = new Customer();
-                    Customer receiver = new Customer();
+                    Customer sender = new Customer(senderId, "sender", "sender", "dateOfBirth", generateId(), 1000);
+                    Customer receiver = new Customer(receiverId, "receiver", "receiver", "dateOfBirth", generateId(),
+                            0);
 
-                    if (sender != null && receiver != null) {
-                        Customer customer = new Customer();
-
-                        customer.performTransfer(sender, accountId, receiver);
-                    } else {
-                        System.err.println("Invalid sender or receiver.");
-                    }
+                    // Perform transfer
+                    sender.performTransfer(sender, transferAmount, receiver);
                 } else {
-
+                    /* further functions */
                 }
             } catch (IllegalStateException e) {
                 System.err.println("Something went wrong. Please try again.");
